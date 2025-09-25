8e4539420d1e05e0dc49b2e287af5cd87000870c
luisdavi29052005 <luisdaviferrer@gmail.com>
2025-09-25 17:48:25 -0300

    speedmetricas

--- index.tsx
+++ index.tsx
@@ -1,16 +1,19 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
+<SpeedInsights />
 const root = ReactDOM.createRoot(rootElement);
 root.render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 );
